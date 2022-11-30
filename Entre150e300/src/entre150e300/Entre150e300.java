9108935812e7391ed4fec0212ee052d9ace4aa44
Lucas Farias <83430368+Fariaslr@users.noreply.github.com>
2022-11-29 22:37:33 -0300

    🔧

---
+++ Entre150e300/src/entre150e300/Entre150e300.java
@@ -0,0 +1,15 @@
+package entre150e300;
+
+/*
+    3- Imprima todos os números de 150 a 300
+ */
+public class Entre150e300 {
+
+    public static void main(String[] args) {
+        
+        for(int i=150; i<300; i++){
+            System.out.printf("%d |",i);
+        }
+    }
+    
+}
